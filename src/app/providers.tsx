8b8022e125124e887b6a5d67a4c56459a6af6144
osy9907 <osy9907@naver.com>
2025-10-31 10:31:04 +0900

    feat: mui 라이브러리 추가 및 전역 provider에 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { ReactNode, useState } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import theme from './theme';
 export default function Providers({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
 
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
         </QueryClientProvider>
     );
 }
